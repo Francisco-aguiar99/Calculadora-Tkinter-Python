c61568a03425c7444e755d14b1771f219ec31bff
francisco_aguiar <francisco_aguiar99@hotmail.com>
2021-11-25 22:55:42 -0400

    estilos dos botões configurações para não aparecer letras

--- calculadora/cal_class.py
+++ calculadora/cal_class.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from typing import List
+import re
 
 
 class Calcu:
@@ -12,8 +13,58 @@ class Calcu:
      ):
         self.root = root
         self.label = label
-        self.disolay = display
+        self.display = display
         self.buttons = buttons
 
     def start(self):
+        self._config_buttons()
+        self._config_display()
         self.root.mainloop()
+
+    def _config_buttons(self):
+        buttons = self.buttons
+        for row_values in buttons:
+            for button in row_values:
+                button_text = button['text']
+
+                if button_text == '«':
+                    button.bind('<Button-1>', self.clear)
+
+                if button_text in '0123456789.+-÷*)(^':
+                    button.bind('<Button-1>', self.add_text_to_display)
+
+                if button_text == '=':
+                    button.bind('<Button-1>', self.calculate)
+
+    def _config_display(self):
+        fixed_text = self._fix_text(self.display.get())
+
+
+    def _fix_text(self, text):
+        # substitui tudo que não for 0123456789./*-+^
+        text = re.sub(r'[^\d\.\÷\*\-\+\^\(\)e]', r'', text, 0)
+        # Substitui sinais repetidos para apenas um sinal
+        text = re.sub(r'([\.\+\÷\-\*\^])\1+', r'\1', text, 0)
+        # Substitui () *() para nada
+        text = re.sub(r'\*?\(\)', '', text)
+        return text
+
+    def clear(self, evento=None):
+        self.display.delete(0, 'end')
+
+    def add_text_to_display(self, event=None):
+        self.display.insert('end', event.widget['text'])
+
+    def calculate(self, event=None):
+        fixed_text = self._fix_text(self.display.get())
+        equations = self._get_equations(fixed_text)
+        try:
+            ...
+        except OverflowError:
+            self.label.config(text='Não consegui realizar essa conta')
+        except Exception as erro:
+            print(f'Esse foi o erro {erro}')
+            self.label.config(text='Conta inválida')
+
+    def _get_equations(self, text):
+        return re.split(r'\^', text, 0)
--- calculadora/factories.py
+++ calculadora/factories.py
@@ -5,7 +5,7 @@ from typing import List
 def style_root():
     root = tk.Tk()
     root.title('Calculadora')
-    root.config(padx=5, pady=10, background='#fff')
+    root.config(padx=5, pady=10, background='#1C0D16')  #293241
     root.resizable(False, False)  # com essa chamanda a janela nao vai ser redinicionada
     # root.geometry('500x450')
     return root
@@ -13,8 +13,8 @@ def style_root():
 
 def style_label(root):
     label = tk.Label(
-        root, text='Sem conta', anchor='e', justify='right',
-        background='#fff'
+        root, text='Expressão atual', anchor='e', justify='right',
+        background='#4F607D'
     )
     label.grid(row=0, column=0, columnspan=5, sticky='news')
     return label
@@ -22,34 +22,35 @@ def style_label(root):
 
 def style_display(root):
     display = tk.Entry(root)
-    display.grid(row=1, column=0, columnspan=5, sticky='news', pady=(0, 10))
+    display.grid(row=1, column=0, columnspan=5, sticky='news', pady=(2, 10))
     display.config(
         font=('Helvetica', 40, 'bold'),
         justify='right', bd=1, relief='flat',
-        highlightthickness=1, highlightcolor='#ccc'
+        highlightthickness=0, highlightcolor='#ccc', background='#56698A'
     )
-    display.bind('<Control-a>', display_control_a)
+    display.bind('<Control-a>', _display_control_a)
     return display
 
 
-def display_control_a(evento):
+def _display_control_a(evento):
     evento.widget.select_range(0, 'end')
     evento.widget.icursor('end')
+    evento.config(background='#EE6C4D')
     return 'brenk'
 
 
 def make_buttons(root) -> List[List[tk.Button]]:
     button_texts: List[List[str]] = [
-        ['7', '8', '9', '+', 'C'],
-        ['4', '5', '6', '-', '/'],
-        ['1', '2', '3', '*', '♀'],
-        ['0', '.', '(', ')', '='],
+        ['7', '8', '9', '=', '«'],
+        ['4', '5', '6', '-', '÷'],
+        ['1', '2', '3', '*', '('],
+        ['0', '.', '^', '+', ')'],
 
     ]
 
     buttons: List[List[tk.Button]] = []
 
-     # estilos dos buttons
+      # estilos dos buttons
     for row, row_value in enumerate(button_texts, start=2):
         button_row = []
         for col_index, col_value in enumerate(row_value):
